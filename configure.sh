f37d23a57780d2813143fee19c7ebce1f227c848
Madhav Achar <acharmad@gmail.com>
2020-12-12 14:21:20 -0500

    Updated main app to use window from submodule lib

--- configure.sh
+++ configure.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
 
-cmake -S . -B build/
+# Many cmake options are the option add a -D (sometimes
+# also seen with a space but prefer without) i.e. in this
+# case the option is GLFW_BUILD_DOCS which is an option
+# that is specified with the option(...) command in the 
+# CMakeLists.txt underneath extern/glfw
+mkdir -p build && cd build && cmake -DGLFW_BUILD_DOCS=OFF ..
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,49 @@
 #include <iostream>
 #include "adder/adder.h"
+#include "GLFW/glfw3.h"
 
 using namespace std;
 
 int main() {
 
     std::cout << "Add 5 and 3: " << add(5, 3) << std::endl;
+
+    GLFWwindow* window;
+    int width, height;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Draw gears
+        // draw();
+
+        // Update animation
+        // animate();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
 
     return 0;
 }
